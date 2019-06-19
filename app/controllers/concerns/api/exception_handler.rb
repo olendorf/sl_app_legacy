34e3f121955d4728b651a49f70af56368f1289c2
Robert Olendorf <drolendorf@gmail.com>
2019-06-19 19:19:49 +0000

    fixed code coverage

--- app/controllers/concerns/api/exception_handler.rb
+++ app/controllers/concerns/api/exception_handler.rb
@@ -8,9 +8,9 @@ module Api
     extend ActiveSupport::Concern
 
     included do
-      rescue_from ActiveRecord::RecordNotFound do |e|
-        json_response({ message: e.message }, :not_found)
-      end
+      # rescue_from ActiveRecord::RecordNotFound do |e|
+      #   json_response({ message: e.message }, :not_found)
+      # end
 
       rescue_from ActionController::BadRequest do |e|
         json_response({ message: e.message }, :bad_request)
