1835e1f2028a23a7f7325462598d11a05e82a6d1
Robert Olendorf <drolendorf@gmail.com>
2019-06-19 13:40:44 +0000

    just triggering travis

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -17,6 +17,7 @@ SimpleCov.start do
   add_filter 'spec/factories'
 end
 
+#
 # Requires supporting ruby files with custom matchers and macros, etc, in
 # spec/support/ and its subdirectories. Files matching `spec/**/*_spec.rb` are
 # run as spec files by default. This means that files in spec/support that end
