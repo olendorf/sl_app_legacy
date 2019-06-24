42a4338b0ef19372348e3df4ea68f5b83f3da39a
Robert Olendorf <drolendorf@gmail.com>
2019-06-24 16:13:08 +0000

    policy set

---
+++ spec/policies/api/v1/rezzable/terminal_policy_spec.rb
@@ -0,0 +1,32 @@
+require 'rails_helper'
+
+RSpec.describe Api::V1::Rezzable::TerminalPolicy, type: :policy do
+  let(:owner) { FactoryBot.create :owner }
+  let(:manager) { FactoryBot.create :manager, 
+                    expiration_date: 1.month.from_now,
+                    account_level: 1
+  }
+  let(:user) { FactoryBot.create :user, 
+                    expiration_date: 1.month.from_now,
+                    account_level: 1
+  }
+  
+  let(:terminal) { FactoryBot.build :terminal }
+
+  subject { described_class }
+
+  permissions :create?, :show?, :update?, :destroy? do
+    it 'grants permission to an owner' do 
+      expect(subject).to permit(owner, terminal)
+    end
+    
+    it 'denies permission to a manager' do 
+      expect(subject).to_not permit(manager, terminal)
+    end 
+     
+    it 'denies permission to a user' do 
+      expect(subject).to_not permit(user , terminal)
+    end 
+    
+  end
+end
