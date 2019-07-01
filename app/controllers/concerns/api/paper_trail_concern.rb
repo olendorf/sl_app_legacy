b30430c58820804c4807a45c88907421198a633d
Robert Olendorf <drolendorf@gmail.com>
2019-07-01 19:12:10 +0000

    a little code coverage fix

---
+++ app/controllers/concerns/api/paper_trail_concern.rb
@@ -0,0 +1,14 @@
+require 'active_support/concern'
+
+module Api
+  # Handles methods associated with paper trail, moslty removes them from
+  # api controller so as not to screw up code coverage
+  module PaperTrailConcern
+    extend ActiveSupport::Concern
+  end
+  
+   # Override paper trail  to get the correct user for the api call
+  def user_for_paper_trail
+    @requesting_object.user_id
+  end
+end
