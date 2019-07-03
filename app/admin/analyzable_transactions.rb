8da5a7969db21259a04748c98a1736e91ce18b74
Robert Olendorf <drolendorf@gmail.com>
2019-07-03 17:38:10 +0000

    transaction show for admin

--- app/admin/analyzable_transactions.rb
+++ app/admin/analyzable_transactions.rb
@@ -2,7 +2,7 @@ ActiveAdmin.register Analyzable::Transaction do
   
   menu label: 'Transactions', parent: 'Data'
   
-  actions :all, except: [:destroy, :new, :create]
+  actions :all, except: [:destroy]
   
   includes :user
   
@@ -38,11 +38,63 @@ ActiveAdmin.register Analyzable::Transaction do
     column 'Description' do |transaction|
       transaction.description.to_s.truncate(15, separator: ' ', omission: '...')
     end
+    column :created_at
     column :alert
     
     actions
     
   end
+  
+  filter :transaction_key, label: 'Transaction ID'
+  filter :user_avatar_name_contains, label: 'User Name'
+  filter :amount
+  filter :category, as: :check_boxes, 
+                    collection: proc { Analyzable::Transaction.categories }
+  filter :description
+  filter :alert
+  filter :created_at
+  
+  show title: proc { 
+    "#{resource.amount.positive? ? "Payer" : "Payee"}: #{resource.target_name}"} do 
+      
+    attributes_table do 
+      row 'Payer' do |transaction|
+        "#{resource.target_name}: (#{resource.target_key})"
+      end if resource.amount.positive?
+      row 'Payee' do |transaction|
+        "#{resource.target_name}: (#{resource.target_key})"
+      end unless resource.amount.positive?
+      row 'Transaction ID', :transaction_key
+      row :amount
+      row :balance
+      row :category
+      row :description
+      row :user do |transaction|
+        link_to transaction.user.avatar_name, admin_user_path(transaction.user)
+      end 
+      row :source do |transaction|
+        transaction.source_link
+      end
+      row :parent_transaction do |resource|
+        link_to resource.parent_transaction.id, 
+                admin_analyzable_transaction_path(resource.parent_transaction)
+      end if resource.parent_transaction
+      row 'Splits' do |transaction|
+        ul do 
+          transaction.sub_transactions.each do |splt|
+            li class: 'list-unstyled' do 
+              link_to "#{splt.amount} -> #{splt.target_name}", admin_analyzable_transaction_path(splt)
+            end
+          end 
+        end
+      end if resource.sub_transactions.size > 0
+      row :alert
+      
+    end
+  end
+  
+  
+  
 # See permitted parameters documentation:
 #
 # permit_params :list, :of, :attributes, :on, :model
