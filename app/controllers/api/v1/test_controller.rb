d59b0c227700b07bdcc8becba8e949dc9ff0cd30
Robert Olendorf <drolendorf@gmail.com>
2019-07-04 22:03:08 +0000

    more tweaks

--- app/controllers/api/v1/test_controller.rb
+++ app/controllers/api/v1/test_controller.rb
@@ -11,7 +11,12 @@ module Api
       def show
         keys = request.headers.to_h.keys.grep /http_x/i
         headers = request.headers.to_h.with_indifferent_access
-        render json: { message: 'OK', headers: headers.slice(*keys) }
+        auth_key = request.headers.to_h.keys.grep(/auth_digest/i).first
+        time_key = request.headers.to_h.keys.grep(/auth_time/i).first
+        sent_headers = headers.slice(*keys)
+        sent_headers[auth_key] = request.headers[auth_key]
+        sent_headers[time_key] = request.headers[time_key]
+        render json: { message: 'OK', headers: sent_headers }
       end
     end
   end
