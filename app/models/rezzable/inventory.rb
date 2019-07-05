b678f9d694cc12275130e5435adfe41d70786ed5
Robert Olendorf <drolendorf@gmail.com>
2019-07-05 14:11:59 +0000

    enum for inventory_type

--- app/models/rezzable/inventory.rb
+++ app/models/rezzable/inventory.rb
@@ -1,3 +1,16 @@
 class Rezzable::Inventory < ApplicationRecord
   belongs_to :server, class_name: 'Rezzable::Server', counter_cache: :inventory_count
+  
+  enum inventory_type: {            texture: 0,
+            sound: 1,
+            landmark: 3,
+            clothing: 5,
+            object: 6,
+            notecard: 7,
+            script: 10,
+            body_part: 13,
+            animation: 20,
+            gesture: 21,
+            setting: 56
+        }
 end
--- spec/models/rezzable/inventory_spec.rb
+++ spec/models/rezzable/inventory_spec.rb
@@ -2,4 +2,18 @@ require 'rails_helper'
 
 RSpec.describe Rezzable::Inventory, type: :model do
   it { should belong_to(:server) }
+  it { should define_enum_for(:inventory_type).with_values(
+            texture: 0,
+            sound: 1,
+            landmark: 3,
+            clothing: 5,
+            object: 6,
+            notecard: 7,
+            script: 10,
+            body_part: 13,
+            animation: 20,
+            gesture: 21,
+            setting: 56
+          )
+  }
 end
