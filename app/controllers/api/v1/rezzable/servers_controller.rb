9134c03526a8d4c6e7bf53f8dbfe3efb302b1ce7
Robert Olendorf <drolendorf@gmail.com>
2019-07-05 18:06:07 +0000

    rubocopped

--- app/controllers/api/v1/rezzable/servers_controller.rb
+++ app/controllers/api/v1/rezzable/servers_controller.rb
@@ -1,8 +1,16 @@
-class Api::V1::Rezzable::ServersController < Api::V1::RezzableController
-  
-  private
-  
-  def response_data
-    {api_key: @requesting_object.api_key}
-  end 
+# frozen_string_literal: true
+
+module Api
+  module V1
+    module Rezzable
+      # Controller for requests coming from in world controllers
+      class ServersController < Api::V1::RezzableController
+        private
+
+        def response_data
+          { api_key: @requesting_object.api_key }
+        end
+      end
+    end
+  end
 end
