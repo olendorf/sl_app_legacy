7138c5c6bd8f40c13a7c950f9f30aaa7ee8c06c6
Robert Olendorf <drolendorf@gmail.com>
2019-07-26 13:41:37 +0000

    rubocopped

--- app/controllers/api/v1/rezzable/vendors_controller.rb
+++ app/controllers/api/v1/rezzable/vendors_controller.rb
@@ -1,11 +1,18 @@
 # frozen_string_literal: true
 
-class Api::V1::Rezzable::VendorsController < Api::V1::RezzableController
-  def response_data
-    {
-      api_key: @requesting_object.api_key,
-      price: @requesting_object.actable.price,
-      image_key: @requesting_object.actable.image_key
-    }
+module Api
+  module V1
+    module Rezzable
+      # Handles requests about inworld Vendors
+      class VendorsController < Api::V1::RezzableController
+        def response_data
+          {
+            api_key: @requesting_object.api_key,
+            price: @requesting_object.actable.price,
+            image_key: @requesting_object.actable.image_key
+          }
+        end
+      end
+    end
   end
 end
--- spec/models/rezzable/vendor_spec.rb
+++ spec/models/rezzable/vendor_spec.rb
@@ -18,7 +18,8 @@ RSpec.describe Rezzable::Vendor, type: :model do
   let(:inventory) { FactoryBot.create :inventory, inventory_name: product.product_name }
 
   let(:vendor) do
-    vendor = FactoryBot.build :vendor, user_id: user.id, inventory_name: inventory.inventory_name
+    vendor = FactoryBot.build :vendor, user_id: user.id,
+                                       inventory_name: inventory.inventory_name
     vendor.save
     server.clients << vendor
     vendor
@@ -26,17 +27,6 @@ RSpec.describe Rezzable::Vendor, type: :model do
   it 'should have weight of 1' do
     expect(vendor.weight).to eq 1
   end
-
-  # it 'should get the price' do
-  #   user = FactoryBot.create :user
-  #   server = FactoryBot.create :server, user_id: user.id
-  #   server.clients << vendor
-  #   inventory = FactoryBot.create :inventory
-  #   product = FactoryBot.create :product, product_name: inventory.inventory_name, user_id: user.id
-  #   vendor.inventory_name = inventory.inventory_name
-  #   server.inventories << inventory
-  #   expect(vendor.price).to eq product.price
-  # end
 
   it 'should get the inventory' do
     expect(vendor.inventory).to eq inventory
